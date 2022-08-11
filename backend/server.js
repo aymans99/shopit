d54fa63372b7ec01660f20fc5fd6c6ae6d6cc6e3
ayman <aymanshaikh42@gmail.com>
2022-08-11 17:13:03 +0530

    change in server for unhandled rejections

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,15 @@ dotenv.config({path:'backend/config/config.env'})
 //connecting to databse
 connectDatabase();
 
-app.listen(process.env.PORT,()=>{
+const server= app.listen(process.env.PORT,()=>{
     console.log(`Server started on PORT:  ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+})
+
+process.on('unhandledRejection',err =>{
+
+    console.log(`ERROR:${err.message}`);
+    console.log('Shutting down the server due to unhandled Promise rejection');
+    server.close(() => {
+        process.exit(1)
+    })
 })
