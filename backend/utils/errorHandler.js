fc0ce6c3fd042356b95252617f20b74c292c374a
ayman <aymanshaikh42@gmail.com>
2022-08-11 16:52:01 +0530

    first commit

---
+++ backend/utils/errorHandler.js
@@ -0,0 +1,10 @@
+//error handler class
+class ErrorHandler extends Error {
+    constructor(message,errorCode){
+        super(message);
+        this.statusCode=this.statusCode
+
+        Error.captureStackTrace(this,this.constructor)
+    }
+}
+module.exports=ErrorHandler;
