3137d14a645ee0c8d18800e5568a0558c1b1ee1c
ayman shaikh <aymanshaikh@algoseedlabs.com>
2023-03-17 17:27:38 +0530

    more functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,9 @@ const User = require("../models/user");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncError = require("../middlewares/catchAsyncerrors");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
+const catchAsyncerrors = require("../middlewares/catchAsyncerrors");
+const crypto = require("crypto");
 
 //Register a user => /api/v1/register
 exports.registerUser = catchAsyncError(async (req, res, next) => {
@@ -82,6 +85,84 @@ exports.forgotPassword = catchAsyncError(async (req, res, next) => {
   }
 });
 
+//Reset Password => /api/v1/password/reset/:token
+
+exports.resetPassword = catchAsyncerrors(async (req, res, next) => {
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  //finding the user with the same passwordtoken
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "Password reset token is invalid or has been expired",
+        400
+      )
+    );
+  }
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password does not match", 400));
+  }
+
+  //setup new password
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+  sendToken(user, 200, res);
+});
+
+//Get currently logged in user details => /api/v1/me
+exports.getUserProfile = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+//update/change password => /api/v1/password/update
+exports.updatePassword = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  //check previous user password
+  const isMatched = await user.comparePassword(req.body.oldPassword);
+  if (!isMatched) {
+    return next(new ErrorHandler("old password is wrong", 400));
+  }
+  //password coming from body through postman
+  user.password = req.body.password;
+  await user.save();
+  sendToken(user, 200, res);
+});
+
+//update user profile => /api/v1/me/update
+exports.updateProfile = catchAsyncError(async (req, res, next) => {
+  //if we just want to change the name then we can do it from frontend rest all will be present just send to the api here we are specifying that name and email should be changed the main things is from front end
+
+  //here we are just checking, if you just want to chnage the name just send name through frontend rest all will be same and hit it
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  //Update avatar : TODO
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+  });
+});
+
 //Logout user => /api/v1/logout
 exports.logout = catchAsyncError(async (req, res, next) => {
   res.cookie("token", null, {
@@ -91,5 +172,71 @@ exports.logout = catchAsyncError(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "Logged out",
+  });
+});
+
+//Admin Routes
+
+//Get all users => /api/v1/admin/users
+exports.allUsers = catchAsyncError(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    sucess: true,
+    users,
+  });
+});
+
+//get user details ie specific user by id => /api/v1/admin/user/:id
+exports.getUserDetails = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not found with this ${req.params.id}`)
+    );
+  }
+  res.status(200).json({
+    sucess: true,
+    user,
+  });
+});
+
+// update user profile => /api/v1/admin/user/:id
+exports.updateUser = catchAsyncError(async (req, res, next) => {
+  //if we just want to change the name then we can do it from frontend rest all will be present just send to the api here we are specifying that name and email should be changed the main things is from front end
+
+  //here we are just checking, if you just want to chnage the name just send name through frontend rest all will be same and hit it
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+  //Update avatar : TODO
+
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+  });
+});
+
+//Delete User => /api/v1/admin/user/:id
+exports.deleteUser = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not found with this ${req.params.id}`)
+    );
+  }
+
+  // remove avatar fron cloudinary : TODO
+  await user.remove();
+  res.status(200).json({
+    sucess: true,
   });
 });
